871ab43386dff613b0d1ae139bcaab6bdcc3b117
Kirill Lashuk <kirill.lashuk@gmail.com>
2015-11-20 00:54:52 -0800

    Investigate

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ gemspec
 gem 'rake'
 gem 'yard'
 gem 'asciidoctor'
+gem 'pry'
--- lib/rgl/dijkstra.rb
+++ lib/rgl/dijkstra.rb
@@ -61,19 +61,35 @@ module RGL
 
     def relax_edges(target = nil, break_on_target = false)
       until @queue.empty?
+        puts "qb: #{@queue.to_a.map(&:vertex)}"
+
         u = @queue.pop
+        puts "poped: #{u}"
 
-        break if break_on_target && u == target
+        if break_on_target && u == target
+          puts "break: #{u}"
+          break
+        end
 
+        puts "ex v: #{u}"
         @visitor.handle_examine_vertex(u)
 
         @graph.each_adjacent(u) do |v|
-          relax_edge(u, v) unless @visitor.finished_vertex?(v)
+          unless @visitor.finished_vertex?(v)
+            puts "rel e: #{[u, v]}"
+            relax_edge(u, v)
+          else
+            puts "else: #{[u, v]}"
+          end
         end
 
         @visitor.color_map[u] = :BLACK
         @visitor.handle_finish_vertex(u)
+
+        puts "qe: #{@queue.to_a.map(&:vertex)}"
       end
+
+      puts "actions: #{@queue.actions}"
     end
 
     def relax_edge(u, v)
@@ -89,13 +105,17 @@ module RGL
 
         if @visitor.color_map[v] == :WHITE
           @visitor.color_map[v] = :GRAY
+          puts "push: #{[v, new_v_distance]}"
           @queue.push(v, new_v_distance)
         elsif @visitor.color_map[v] == :GRAY
+          puts "dec: #{[v, new_v_distance]}"
           @queue.decrease_key(v, old_v_distance, new_v_distance)
         end
-
+ 
+        puts "relaxed: #{[u, v]}"
         @visitor.handle_edge_relaxed(u, v)
       else
+        puts "not relaxed: #{[u, v]}"
         @visitor.handle_edge_not_relaxed(u, v)
       end
     end
@@ -106,21 +126,34 @@ module RGL
 
     class Queue < SimpleDelegator # :nodoc:
 
+      attr_reader :actions
+
       def initialize
         @heap = Containers::Heap.new { |a, b| a.distance < b.distance }
+        @actions = []
         super(@heap)
       end
 
       def push(vertex, distance)
+        @actions << [vertex, distance]
         @heap.push(vertex_key(vertex, distance), vertex)
       end
 
+      def pop
+        @heap.pop.tap { |v| @actions << -v }
+      end
+
       def decrease_key(vertex, old_distance, new_distance)
+        raise "wut"
         @heap.change_key(vertex_key(vertex, old_distance), vertex_key(vertex, new_distance))
       end
 
       def vertex_key(vertex, distance)
         VertexKey.new(vertex, distance)
+      end
+
+      def to_a
+        @heap.instance_variable_get(:@stored).keys.select { |k| @heap.has_key?(k) }
       end
 
       VertexKey = Struct.new(:vertex, :distance)
@@ -156,3 +189,46 @@ module RGL
   end # module Graph
 
 end # module RGL
+
+__END__
+
+irb -Ilib -rrgl/base -rrgl/adjacency -rrgl/dijkstra
+
+graph = RGL::AdjacencyGraph[1,2, 1,3, 1,4, 1,5, 2,6, 3,8, 5,7, 9,10, 9,11, 9,12, 10,14, 11,6, 12,13, 13,16]
+graph.dijkstra_shortest_path(Hash.new(1), 6, 16)
+
+def wut
+  require 'pry'
+  actions = [[6, 0], -6, [2, 1], [11, 1], -2, [1, 2], -11, [9, 2], -1, [3, 3], [4, 3], [5, 3], -9, [10, 3], [12, 3], -3, [8, 4], -5, [7, 4], -12, [13, 4], -10, [14, 4], -4, -14, -8, -7, -7]
+
+  heap = Containers::Heap.new { |a, b| a.distance < b.distance }
+
+  class Containers::Heap
+    def vertices
+      @stored.keys.select { |k| has_key?(k) }.map(&:vertex)
+    end
+
+    def stored
+      @stored
+    end
+  end
+
+  VertexKey = Struct.new(:vertex, :distance)
+
+  actions.each do |action|
+    if action.is_a?(Array)
+      puts "pushing #{action}"
+      v, d = action
+      heap.push(VertexKey.new(*v, d), v)
+      puts "verts: #{heap.vertices}"
+      puts
+      binding.pry if p == 7
+    else
+      puts "stored: #{heap.stored[VertexKey.new(13, 4)]}"
+      p = heap.pop
+      puts "popped #{p} (expected #{-action})"
+      puts "verts: #{heap.vertices}"
+      binding.pry if p == 7
+    end
+  end
+end
